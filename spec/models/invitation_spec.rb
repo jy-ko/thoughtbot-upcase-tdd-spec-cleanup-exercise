d63af1dadb31cfc5b6b0f916675a9e01a69402c2
jy-ko <jclareko@gmail.com>
2022-12-30 18:13:04 +0200

    made tests pass

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,37 +1,32 @@
 require "rails_helper"
 
 RSpec.describe Invitation do
-  let(:invitation) { build(:invitation, team: team, user: new_user) }
-  let(:new_user) { create(:user, email: "rookie@example.com") }
-  let(:team) { create(:team, name: "A fine team") }
-  let(:team_owner) { create(:user) }
-
-  before do
-    team.update!(owner: team_owner)
-    team_owner.update!(team: team)
-  end
-
   describe "callbacks" do
     describe "after_save" do
       context "with valid data" do
         it "invites the user" do
-          invitation.save
+          new_user = User.create(email: "rookie@example.com")
+          team_owner = User.create
+          team = Team.create(name:"A fine team")
+          invitation = Invitation.create(team: team, user: new_user)
           expect(new_user).to be_invited
         end
       end
 
       context "with invalid data" do
-        before do
+        it "does not save the invitation" do
+          new_user = User.create(email: "rookie@example.com")
+          team_owner = User.create
+          team = Team.create(name:"A fine team")
+          invitation = Invitation.new(team: team, user: new_user)
           invitation.team = nil
           invitation.save
-        end
-
-        it "does not save the invitation" do
           expect(invitation).not_to be_valid
           expect(invitation).to be_new_record
         end
 
         it "does not mark the user as invited" do
+          new_user = User.create(email: "rookie@example.com")
           expect(new_user).not_to be_invited
         end
       end
@@ -40,16 +35,21 @@ RSpec.describe Invitation do
 
   describe "#event_log_statement" do
     context "when the record is saved" do
-      before do
-        invitation.save
-      end
-
+      
       it "include the name of the team" do
+        new_user = User.create(email: "rookie@example.com")
+        team_owner = User.create
+        team = Team.create(name:"A fine team")
+        invitation = Invitation.new(team: team, user: new_user)
         log_statement = invitation.event_log_statement
         expect(log_statement).to include("A fine team")
       end
 
       it "include the email of the invitee" do
+        new_user = User.create(email: "rookie@example.com")
+        team_owner = User.create
+        team = Team.create(name:"A fine team")
+        invitation = Invitation.new(team: team, user: new_user)
         log_statement = invitation.event_log_statement
         expect(log_statement).to include("rookie@example.com")
       end
@@ -57,16 +57,28 @@ RSpec.describe Invitation do
 
     context "when the record is not saved but valid" do
       it "includes the name of the team" do
+        new_user = User.create(email: "rookie@example.com")
+        team_owner = User.create
+        team = Team.create(name:"A fine team")
+        invitation = Invitation.new(team: team, user: new_user)
         log_statement = invitation.event_log_statement
         expect(log_statement).to include("A fine team")
       end
 
       it "includes the email of the invitee" do
+        new_user = User.create(email: "rookie@example.com")
+        team_owner = User.create
+        team = Team.create(name:"A fine team")
+        invitation = Invitation.new(team: team, user: new_user)
         log_statement = invitation.event_log_statement
         expect(log_statement).to include("rookie@example.com")
       end
 
       it "includes the word 'PENDING'" do
+        new_user = User.create(email: "rookie@example.com")
+        team_owner = User.create
+        team = Team.create(name:"A fine team")
+        invitation = Invitation.new(team: team, user: new_user)
         log_statement = invitation.event_log_statement
         expect(log_statement).to include("PENDING")
       end
@@ -74,6 +86,10 @@ RSpec.describe Invitation do
 
     context "when the record is not saved and not valid" do
       it "includes INVALID" do
+        new_user = User.create(email: "rookie@example.com")
+        team_owner = User.create
+        team = Team.create(name:"A fine team")
+        invitation = Invitation.new(team: team, user: new_user)
         invitation.user = nil
         log_statement = invitation.event_log_statement
         expect(log_statement).to include("INVALID")
